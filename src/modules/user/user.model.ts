fd2358738c117d28b8d455845971d06b90f43e3e
satwik <mohanty.satwik290@gmail.com>
2026-02-25 20:59:28 +0530

    add user schema with type safety

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export type UserRole = "student" | "professor";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ["student", "professor"],
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
