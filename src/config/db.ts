1bc68843ba47a794df564c60381bb00e73795003
satwik <mohanty.satwik290@gmail.com>
2026-02-25 20:47:38 +0530

    connect server with mongoDB

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
+    } catch (error) {
+        console.error("❌ Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    // await connectDB();
-    // console.log("✅ MongoDB Connected");
+    await connectDB();
+    console.log("✅ MongoDB Connected");
 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
