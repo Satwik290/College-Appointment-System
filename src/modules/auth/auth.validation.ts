cc69008b699ecae65b8c0a8424a1e285d6895485
satwik <mohanty.satwik290@gmail.com>
2026-02-28 12:38:22 +0530

    fix  zod validation error

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -4,7 +4,7 @@ export const registerSchema = z.object({
   name: z.string().min(2, {
      message: "Name must be at least 2 characters"
     }),  
-  email: z.string().email({
+  email: z.email({
      message: "Invalid email address" 
     }),
   password: z.string().min(6, {
@@ -14,7 +14,7 @@ export const registerSchema = z.object({
 });
 
 export const loginSchema = z.object({
-  email: z.string().email({
+  email: z.email({
      message: "Invalid email address"
     }),
   password: z.string().min(6, {
--- src/modules/availability/availability.validation.ts
+++ src/modules/availability/availability.validation.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const createAvailabilitySchema = z.object({
-  startTime: z.string().datetime(),
-  endTime: z.string().datetime()
+  startTime: z.iso.datetime(),
+  endTime: z.iso.datetime()
 });
 
 export type CreateAvailabilityInput =
